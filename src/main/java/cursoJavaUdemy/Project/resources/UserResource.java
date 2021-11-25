bd74323626be200b3bc3cac7979856329d812b9d
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2021-11-25 11:58:21 -0300

    Service layer, component registration

--- src/main/java/cursoJavaUdemy/Project/resources/UserResource.java
+++ src/main/java/cursoJavaUdemy/Project/resources/UserResource.java
@@ -1,17 +1,25 @@
 package cursoJavaUdemy.Project.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cursoJavaUdemy.Project.entities.User;
+import cursoJavaUdemy.Project.services.UserService;
 
  
 //O @RestController serve para indicar que esta classe é implementada por um controlador rest
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 	
 	/*	
 		O @GetMapping serve para indicar que esse metodo responde a requisição do tipo get do http
@@ -21,9 +29,16 @@ public class UserResource {
 	*/
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "maria", "maria@gmail.com", "888888888", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	//@PathVariable serve para que o spring entenda que esse id corresponde ao id citado como value no @GetMapping
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
