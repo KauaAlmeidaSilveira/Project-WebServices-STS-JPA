4a19d4f49ba96d645710ecb484b54f1181c078c8
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2021-11-26 12:14:08 -0300

    OrderStatus enum

--- src/main/java/cursoJavaUdemy/Project/config/TestConfig.java
+++ src/main/java/cursoJavaUdemy/Project/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import cursoJavaUdemy.Project.entities.Order;
 import cursoJavaUdemy.Project.entities.User;
+import cursoJavaUdemy.Project.entities.enums.OrderStatus;
 import cursoJavaUdemy.Project.repositories.OrderRepository;
 import cursoJavaUdemy.Project.repositories.UserRepository;
 
@@ -43,9 +44,9 @@ public class TestConfig implements CommandLineRunner{
 		Para que o saveAll funcione é necessario que os valores sejam colocados dentro de uma lista
 		*/
 	
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
