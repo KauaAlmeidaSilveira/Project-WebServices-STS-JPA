6ce5dd6401ac2065abde0d6bdfe25ee6cec5c351
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2021-11-29 12:12:37 -0300

    User insert

--- src/main/java/cursoJavaUdemy/Project/resources/UserResource.java
+++ src/main/java/cursoJavaUdemy/Project/resources/UserResource.java
@@ -1,13 +1,17 @@
 package cursoJavaUdemy.Project.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import cursoJavaUdemy.Project.entities.User;
 import cursoJavaUdemy.Project.services.UserService;
@@ -39,6 +43,14 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		//É NECESSARIO A CRIAÇÃO DA VARIAVEL URI POIS O ResponseEntity.created ESPERA UM DADO DO TIPO URI
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 }
